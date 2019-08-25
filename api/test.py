46ecbfb5670ee4ff8afd50710839d64d899cf190
zekaio <45752994+zekaio@users.noreply.github.com>
2019-08-25 18:40:28 +0800

    Delete test.py

--- api/test.py
+++
@@ -1,8 +0,0 @@
-from common.database import database
-
-# database.updateInfo("333", "凌泽楷", "18520775522")
-
-a = database()
-# print("!")
-a.updateInfo("333", "凌泽楷", "18520775522")
-print(a.getInfo("333"))
