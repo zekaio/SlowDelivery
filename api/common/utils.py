d7ca3e1348fdfcda5d857e634d384220d8c31b66
zekai <zekailing@gmail.com>
2019-09-03 00:14:02 +0800

    Update utils.py

--- api/common/utils.py
+++ api/common/utils.py
@@ -31,27 +31,27 @@ def checkLogin():
 
 # 判断用户是否关注公众号
 def checkSubscribe(open_id):
-    # headers = {
-    #     'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"}
-    # response = requests.get(
-    #     "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + cfg['APPID'] + "&secret=" + cfg[
-    #         'AppSecret'], headers=headers).text
-    # if "errcode" in response:
-    #     abort(401, message="APPID is invalid.")
-    #     return False
-    # else:
-    #     assess_token = response['access_token']
-    # userinfo = requests.get(
-    #     "https://api.weixin.qq.com/cgi-bin/user/info?access_token=" + assess_token + "&openid=" + open_id + "&lang=zh_CN").text
-    # if "subscribe" in userinfo:
-    #     subscribe = userinfo['subscribe']
-    #     if subscribe:
-    #         return True
-    #     else:
-    #         abort(407, message="Please subscribe first.")
-    #         return False
+    headers = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"}
+    response = requests.get(
+        "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + cfg['APPID'] + "&secret=" + cfg[
+            'AppSecret'], headers=headers).text
+    if "errcode" in response:
+        abort(401, message="APPID is invalid.")
+        return False
+    else:
+        assess_token = response['access_token']
+    userinfo = requests.get(
+        "https://api.weixin.qq.com/cgi-bin/user/info?access_token=" + assess_token + "&openid=" + open_id + "&lang=zh_CN").text
+    if "subscribe" in userinfo:
+        subscribe = userinfo['subscribe']
+        if subscribe:
+            return True
+        else:
+            abort(407, message="Please subscribe first.")
+            return False
 
     """
     for test
     """
-    return True
+    # return True
