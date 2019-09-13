696e70b00c44a83a71b16fee62902125010f541c
zekai <zekailing@gmail.com>
2019-09-13 18:57:41 +0800

    。

--- api/app.py
+++ api/app.py
@@ -13,7 +13,7 @@ from resources.updateInfo import updateInfo
 from resources.checkTimeCapsule import checkTimeCapsule
 from resources.checkFlag import checkFlag
 from werkzeug.middleware.proxy_fix import ProxyFix
-import uuid
+
 app = flask.Flask(__name__)
 CORS(app, resources=r'/*', supports_credentials=True)
 app.secret_key = cfg["secret_key"]
@@ -29,17 +29,7 @@ api.add_resource(sendFlag, '/sendFlag')
 api.add_resource(updateInfo, '/updateInfo')
 api.add_resource(checkTimeCapsule, '/checkTimeCapsule')
 api.add_resource(checkFlag, '/checkFlag')
-# for test
-class setSession(flask_restful.Resource):
-    def get(self):
-        if "open_id" not in flask.session:
-            flask.session['open_id'] = "test_open_id_" + str(uuid.uuid4())
-        return {
-            "open_id": flask.session['open_id']
-        }
 
-
-api.add_resource(setSession, '/setSession')
 if __name__ == '__main__':
-    # app.wsgi_app = ProxyFix(app.wsgi_app)
+    app.wsgi_app = ProxyFix(app.wsgi_app)
     app.run(port=8765)
