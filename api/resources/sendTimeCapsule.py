593d78bdf46fe2312fe469bffcc65f75345a5fe2
zekai <zekailing@gmail.com>
2019-09-18 00:40:23 +0800

    Update sendTimeCapsule.py

--- api/resources/sendTimeCapsule.py
+++ api/resources/sendTimeCapsule.py
@@ -51,10 +51,10 @@ class sendTimeCapsule(Resource):
                     msg = data['message']
                 except:
                     abort(400, message="参数错误。")
-            if(data['send_offline']):
-                obj.sendTimeCapsule(openId, data['type'], msg, data['time'],data['send_offline'],data['address'])
-            else:
-                obj.sendTimeCapsule(openId, data['type'], msg, data['time'],data['send_offline'])
+        if data['send_offline']:
+            obj.sendTimeCapsule(openId, data['type'], msg, data['time'], data['send_offline'], data['address'])
+        else:
+            obj.sendTimeCapsule(openId, data['type'], msg, data['time'], data['send_offline'])
         return jsonify({
             "errcode": 0,
             "errmsg": ""
