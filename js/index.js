df9f5a97341b0b173b5c44d81ab36230a2b48a50
gajon <651291702@qq.com>
2019-09-18 11:42:58 +0800

    简单封装对话函数,改善对话UI

--- js/index.js
+++ js/index.js
@@ -21,8 +21,11 @@ axios
     }
   });
 
-var tt = 0; //记录说话时间
-var k = 0; //第二次及以后下滑不再重新执行动画
+let section = [
+  "欢迎光临木棉邮局，这里是毕业慢递专栏",
+  "我们为您的理想和初心提供了寄存慢递服务，以岁月为报，以时光为酬，也帮助你插上前行的路标",
+  "在此之前，请先在这份清单署上你的名字，订立你与未来的契约吧~"
+];
 var mySwiper = new Swiper(".swiper-container", {
   direction: "vertical",
   loop: false,
@@ -30,70 +33,15 @@ var mySwiper = new Swiper(".swiper-container", {
     loadPreNext: true,
     loadOnTransitionStart: true
   },
-  on: {
-    transitionEnd: function() {
-      k++;
-      if (k == 1) {
-        $("#w1").html("欢迎光临木棉邮局，这里是毕业慢递专栏");
-        $("#w1").addClass("word ty");
-        clock = setInterval("count()", 1000);
-      }
-    }
-  }
+  on: {}
 });
 
-function count() {
-  tt++;
-}
 var status = 0; //第几段话
 var infoCompleted = false;
 var firstQueryInfo = true;
-function change() {
-  if (status == 0 && tt >= 2) {
-    clearInterval(clock);
-    tt = 0;
-    clock = setInterval("count()", 1000);
-    $("#w1").removeClass("word ty");
-    $("#w1").html("我们为您的理想和初心提供了寄存慢递服务,");
-    $("#w2").html("以岁月为报，以时光为酬，也帮助你插上前");
-    $("#w3").html("行的路标");
-    setTimeout(function() {
-      $("#w1").addClass("word ty");
-      $("#w2").addClass("word ty ty2");
-      $("#w3").addClass("word ty ty3");
-    }, 1);
-    status = 1;
-  }
-  if (firstQueryInfo) {
-    firstQueryInfo = false;
-    axios.get(prefix + "getInfo").then(function(res) {
-      if (res.data.record) infoCompleted = true;
-    });
-  }
-  if (status == 1 && tt >= 5) {
-    clearInterval(clock);
-    tt = 0;
-    clock = setInterval("count()", 1000);
-    $("#w1").removeClass("word ty");
-    $("#w2").removeClass("word ty ty2");
-    $("#w1").html("在此之前，请先在这份清单上写下自己想说的话，");
-    $("#w2").html("订立你与未来的契约吧~");
-    $("#w3").html("");
-    $("#w4").html("");
-    setTimeout(function() {
-      $("#w1").addClass("word ty");
-      $("#w2").addClass("word ty ty2");
-    }, 1);
-    status = 2;
-  }
-  if (status == 2 && tt >= 3) {
-    clearInterval(clock);
-    tt = 0;
-    if (infoCompleted) window.location.href = "intro.html";
-    else window.location.href = "info.html";
-  }
-}
+initChangeHandle("content", () => {});
 
 function next() {
   mySwiper.slideNext();
+  setTimeout(change, 300);
 }
--- js/offline-index.js
+++ js/offline-index.js
@@ -1,33 +1,20 @@
-var tt=0;  
-var mySwiper = new Swiper('.swiper-container', {
-    direction: 'vertical',
-    loop: false,
-    lazy:{
-        loadPreNext:true,
-        loadOnTransitionStart: true,
-    },
-    on:{
-        transitionEnd:function(){
-            $("#w1").html("欢迎参加开学季木棉邮局——毕业慢递！");
-            $("#w2").html("为了将来信件能顺利寄出，");
-            $("#w3").html("请认真填写以下信息！");
-            $("#w1").addClass("word ty");
-            $("#w2").addClass("word ty ty2");
-            $("#w3").addClass("word ty ty3");
-            clock=setInterval("count()",1000);
-        }
-    }
+let section = [
+  "欢迎参加开学季木棉邮局——毕业慢递！为了将来信件能顺利寄出，请认真填写以下信息！"
+];
+
+var mySwiper = new Swiper(".swiper-container", {
+  direction: "vertical",
+  loop: false,
+  lazy: {
+    loadPreNext: true,
+    loadOnTransitionStart: true
+  },
+  on: {}
 });
-function count(){
-    tt++;
-    console.log(tt);
-}
-function change(){
-    if (tt>=2){
-        clearInterval(clock);tt=0;
-        window.location.href="offline-info.html";
-    }
-}
+initChangeHandle("content", () => {
+  window.location.href = "offline-info.html";
+});
 function next() {
-    mySwiper.slideNext();
+  mySwiper.slideNext();
+  change();
 }
