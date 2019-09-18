da25adfa3b0e1eea5209c01a2d660f054f57c002
zekai <zekailing@gmail.com>
2019-09-18 13:01:11 +0800

    getInfo添加flag、text、voice判断

--- api/resources/getInfo.py
+++ api/resources/getInfo.py
@@ -2,7 +2,7 @@
 from flask import jsonify
 from flask_restful import Resource
 from common.database import database
-from common.utils import checkSubscribe,checkLogin
+from common.utils import checkSubscribe, checkLogin
 
 
 class getInfo(Resource):
@@ -12,10 +12,14 @@ class getInfo(Resource):
         obj = database()
         info = obj.getInfo(openId)
         if info:
+            check = obj.checkTimeCapsule(openId)
             return jsonify({
                 "record": True,
                 "name": info['name'],
-                "tel": info['tel']
+                "tel": info['tel'],
+                "check_flag": obj.checkFlag(openId),
+                "check_text": check['check_text'],
+                "check_voice": check['check_voice']
             })
         else:
             return jsonify({
